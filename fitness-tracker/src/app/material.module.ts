563ffe8f399306ab0775c9c2b95a4a77dafb74fa
Mario Jonckers <mario_jonckers@peopleware.be>
2018-04-11 10:28:51 +0200

    Our First Angular Material Component - The Button

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule
+  ]
 })
 export class MaterialModule { }
