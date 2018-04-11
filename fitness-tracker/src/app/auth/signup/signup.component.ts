98defeb14b2c05521f91f21dee2663c73106b2f9
Mario Jonckers <mario_jonckers@peopleware.be>
2018-04-11 11:39:39 +0200

    Restricting Pickable Dates

--- fitness-tracker/src/app/auth/signup/signup.component.ts
+++ fitness-tracker/src/app/auth/signup/signup.component.ts
@@ -7,10 +7,13 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  private maxDate: Date;
 
   constructor() { }
 
   ngOnInit() {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   private onSubmit(form: NgForm): void {
