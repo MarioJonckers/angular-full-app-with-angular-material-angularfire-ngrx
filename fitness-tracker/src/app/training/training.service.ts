441d916d57b4ab195a590f228f47bdc6eb689904
Mario Jonckers <mario.jonckers@gmail.com>
2018-04-12 14:39:49 +0200

    Preparing the Exercise Data

---
+++ fitness-tracker/src/app/training/training.service.ts
@@ -0,0 +1,10 @@
+import { Exercise } from './exercise.model';
+
+export class TrainingService {
+  private availableExercises: Exercise[] = [
+    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
+    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
+    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
+  ];
+}
