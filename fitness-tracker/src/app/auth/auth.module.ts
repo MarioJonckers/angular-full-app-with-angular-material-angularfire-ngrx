ad2bb0404369f7aee50d2ea85dd5f94a20509633
Mario Jonckers <mario_jonckers@peopleware.be>
2018-04-13 13:27:34 +0200

    Splitting Up Routes

--- fitness-tracker/src/app/auth/auth.module.ts
+++ fitness-tracker/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     SharedModule,
+    AuthRoutingModule,
     ReactiveFormsModule,
     AngularFireAuthModule
   ],
