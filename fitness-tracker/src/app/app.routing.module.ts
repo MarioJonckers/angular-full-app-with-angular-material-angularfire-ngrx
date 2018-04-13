98f5f0309a9e11efeeaf71385bc562b62a614d92
Mario Jonckers <mario_jonckers@peopleware.be>
2018-04-13 13:49:11 +0200

    Moving the Auth Guard

--- fitness-tracker/src/app/app.routing.module.ts
+++ fitness-tracker/src/app/app.routing.module.ts
@@ -5,7 +5,7 @@ import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
-  { path: 'training', loadChildren: './training/training.module#TrainingModule' }
+  { path: 'training', loadChildren: './training/training.module#TrainingModule', canLoad: [AuthGuard] }
 ];
 
 @NgModule({
--- fitness-tracker/src/app/training/training-routing.module.ts
+++ fitness-tracker/src/app/training/training-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { AuthGuard } from '../auth/auth.guard';
 import { TrainingComponent } from './training.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: TrainingComponent, canActivate: [AuthGuard] }
+  { path: '', component: TrainingComponent }
 ];
 
 @NgModule({
